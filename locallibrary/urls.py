c1a3eaffccd9040fba5794539c7f035b310c9454
cecaro <rbcerovic@gmail.com>
2018-04-18 22:19:45 +0200

    Gitignore and url redirect for empty url

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -33,3 +33,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
