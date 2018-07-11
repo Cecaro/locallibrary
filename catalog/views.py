8c2d107ea90389d70ca50e3ae55a6b8f6d0e0740
Romain Cerovic <rbcerovic@gmail.com>
2018-07-11 17:09:47 +0200

    Pass genre count to the index view

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
+
 def index(request):
     """
     View function for the index page
@@ -10,12 +11,15 @@ def index(request):
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
     # Available books (status = a)
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
 
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres},
     )
