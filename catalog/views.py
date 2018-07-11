ab7f4dc6c048aa8684af434c865e6a130ff57bdf
Romain Cerovic <rbcerovic@gmail.com>
2018-07-11 18:33:17 +0200

    Halfway through tutorial part 6
    Done the book list view and the book detail view

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
@@ -23,3 +24,11 @@ def index(request):
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
