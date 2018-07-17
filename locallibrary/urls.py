42c5052fda1c5131b617b6d90ec68575c731ff7a
Romain Cerovic <rbcerovic@gmail.com>
2018-07-17 17:44:55 +0200

    Login and logout - Authentification
    Halfway through tutorial 8

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-     path('catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
@@ -34,8 +34,13 @@ from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-#Add URL maps to redirect the base URL to our application
+# Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
